a8b5f4253a510f7ab9071c359e35b20998e7b216
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2022-05-15 16:07:53 +0300

    made a comic-slider on the main page

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const btns = document.querySelectorAll('.controls__button'),
 			modalOverlay = document.querySelector('.modal__overlay'),
 			modals = document.querySelectorAll('.modal');
 
-// Открытие модальных окон при нажатии кнопок
+/* Открытие модальных окон при нажатии кнопок */
 btns.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		let path = e.currentTarget.getAttribute('data-path');
@@ -41,7 +41,7 @@ btns.forEach((el) => {
 		modalOverlay.classList.add('modal-overlay--visible');
 	});
 });
-// Модальное окно закрывается при нажатие клавиши Esc
+/* Модальное окно закрывается при нажатие клавиши Esc */
 btns.forEach((el) => {
 	el.addEventListener('keydown', (e) =>{
 	if(e.code === "Escape"){
@@ -52,7 +52,7 @@ btns.forEach((el) => {
 	}
 	});
 });
-// Модельное окно закрывается при клике не в рамках модального окна
+/* Модельное окно закрывается при клике не в рамках модального окна */
 modalOverlay.addEventListener('click', (e) => {
 	if (e.target == modalOverlay) {
 		modalOverlay.classList.remove('modal-overlay--visible');
@@ -62,3 +62,42 @@ modalOverlay.addEventListener('click', (e) => {
 	}
 });
 
+
+
+// Slider
+/* Индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+	let i;
+	const slides = document.getElementsByClassName("comic-slider__item");
+	const dots = document.getElementsByClassName("comic-slider__dot");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" comic-slider__dot--active", "");
+	}
+	slides[slideIndex - 1].style.display = "flex";
+	dots[slideIndex - 1].className += " comic-slider__dot--active";
+}
